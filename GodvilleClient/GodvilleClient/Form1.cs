9080fc7f7edc7ee1658fa1b59997a324e68cdce7
Elizaveta Belozerova <belozerova.lizaveta@gmail.com>
2020-12-10 03:17:47 +0500

    simple grpc with winforms example

---
+++ GodvilleClient/GodvilleClient/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using SimpleGrpcService;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GodvilleClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitAsync();
+
+        }
+
+        async Task InitAsync()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            string name = "dhbdt";
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+            MessageBox.Show(reply.Message);
+        }
+    }
+}
