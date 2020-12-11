d44a8260439dde8735cd45d42847f3b2854e888f
Elizaveta Belozerova <belozerova.lizaveta@gmail.com>
2020-12-11 15:48:52 +0500

    login and register almost complete

--- GodvilleClient/GodvilleClient/Program.cs
+++ GodvilleClient/GodvilleClient/Program.cs
@@ -15,7 +15,6 @@ namespace GodvilleClient
 {
     static class Program
     {
-
         static readonly string ip = GetLocalIPAddress();
         /// <summary>
         ///  The main entry point for the application.
@@ -27,22 +26,21 @@ namespace GodvilleClient
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            int myId = TryGetMyId(Model.Config.MyIdFilePath);
+            int myId = MyId.TryGetMyId();
             Model.LoginData loginData = new Model.LoginData();
-            if (myId == -1)
+            while (myId == -1)
             {
                 LoginForm loginForm = new LoginForm(loginData);
                 DialogResult result = loginForm.ShowDialog();
                 if (result == DialogResult.OK)
                 {
                     // найти живых и выбрать среди них случайного диспетчера 
-                    
-                    using var channel = GrpcChannel.ForAddress("http://192.168.100.6:5000");
-
+                    using var channel = GrpcChannel.ForAddress(Model.Config.DispatcherList[0]);
                     var client = new GodvilleServiceClient(channel);
+                    string serverIp;
                     try
                     {
-                        string serverIp = client.Login(
+                        serverIp = client.Login(
                             new LoginData
                             {
                                 Login = loginData.Login,
@@ -52,18 +50,17 @@ namespace GodvilleClient
                     }
                     catch (Exception e)
                     {
-                        MessageBox.Show(e.Message);
+                        Logger.AddErrorMessage(e.Message);
                         return;
                     }
-                    // создать сокет, который слушает этот ip
-                    // получить от сервера через сокет свой id, если все успешно (внутри реализовать "дай мне другой сервер, если этот не отвечает")
-                    // if (myId == -1)
-                    //      MessageBox.Show("Неверное имя пользователя или пароль");
-                    // повторить, пока не успех (не if, a while)
+                    myId = ConnectServerCheckMyId(serverIp);
+                    if (myId == -1)
+                        MessageBox.Show("Неверное имя пользователя или пароль");
+                    else
+                        MyId.SetMyId(myId);
                 }
                 else if (result == DialogResult.Ignore)
                 {
-
                     Model.RegisterData regData = new Model.RegisterData();
                     RegisterForm rf = new RegisterForm(regData);
                     if (rf.ShowDialog() == DialogResult.OK)
@@ -72,15 +69,21 @@ namespace GodvilleClient
                         using var channel = GrpcChannel.ForAddress(Model.Config.DispatcherList[0]);
                         var client = new GodvilleServiceClient(channel);
 
-                        string serverIp = client.Register(
-                            new RegisterData
-                            {
-                                LoginData = new LoginData { Login = regData.Login, Password = regData.Password, ClientIp = ip },
-                                Nickname = regData.Nickname
-                            }).Ip;
-
-                        // создать сокет, который слушает этот ip
-                        // получить от сервера через сокет свой id, если все успешно (внутри реализовать "дай мне другой сервер, если этот не отвечает")
+                        string serverIp;
+                        try
+                        {
+                            serverIp = client.Register(
+                                new RegisterData
+                                {
+                                    LoginData = new LoginData { Login = regData.Login, Password = regData.Password, ClientIp = ip },
+                                    Nickname = regData.Nickname
+                                }).Ip;
+                        } catch(Exception e)
+                        {
+                            Logger.AddErrorMessage(e.Message);
+                            return;
+                        }
+                        myId = ConnectServerCheckMyId(serverIp);
                     }
                     else
                         return;
@@ -90,23 +93,7 @@ namespace GodvilleClient
             }
             Application.Run(new StartForm());
         }
-        static int TryGetMyId(string path)
-        {
-            int myId = -1;
-            try
-            {
-                StreamReader sr = new StreamReader(path);
-                string line = sr.ReadLine();
-                if (string.IsNullOrEmpty(line))
-                    return myId;
-                myId = int.Parse(line);
-            }
-            catch (Exception e)
-            {
-                Logger.AddErrorMessage(e.Message);
-            }
-            return myId;
-        }
+        
         public static string GetLocalIPAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
@@ -118,6 +105,30 @@ namespace GodvilleClient
                 }
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        static int ConnectServerCheckMyId(string serverAddres)
+        {
+            int myId = -1;
+            var lines = serverAddres.Split(":");
+            try
+            {
+                using (TcpClient tcpClient = new TcpClient(lines[0], int.Parse(lines[1])))
+                {
+                    using (NetworkStream networkStream = tcpClient.GetStream())
+                    {
+                        using (StreamReader sr = new StreamReader(networkStream))
+                        {
+                            string input = sr.ReadLine();
+                            myId = int.Parse(input);
+                        }
+                    }
+                }
+            } catch(Exception e)
+            {
+                Logger.AddErrorMessage(e.Message);
+            }
+            return myId;
         }
     }
 }
